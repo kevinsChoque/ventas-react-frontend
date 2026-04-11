677e528e7bfc3f426de2027ab465d637ed12cd57
kevins <kevins.choque@gmail.com>
2026-04-11 17:24:55 -0500

    feat(pruebas): Se creo una poagina para pruebas

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useSnackbar } from 'notistack';
 
 const Test = () => {
-    
+  const { enqueueSnackbar } = useSnackbar();
+  const mostrarNotificacion = () => {
+    enqueueSnackbar('¡Notificación de prueba exitosa!', { variant: 'success' });
+  };
   return (
     <div className="container mt-4">
       <h1>test</h1>
+      <button className="btn btn-primary mt-3" onClick={mostrarNotificacion}>
+        Mostrar notificación
+      </button>
     </div>
   );
 };
-
 export default Test;
